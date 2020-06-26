9ab765ede7dfa5f63cddda9a07a56ad488d39136
e <zhangnan@huarongjf.com>
2020-06-26 11:25:31 +0800

    3.2 布局

---
+++ 3.2 layout/app/src/main/java/com/example/layout/linearLayout.java
@@ -0,0 +1,14 @@
+package com.example.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class linearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
