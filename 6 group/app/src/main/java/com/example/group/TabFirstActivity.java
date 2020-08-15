c98df938465d4ee35a0268ff6dd0e6b3ed611c06
xiafuxiangfei <xiafuxiangfei@sina.com>
2020-08-15 20:31:10 +0800

    基于TabActivity的底部标签栏

---
+++ 6 group/app/src/main/java/com/example/group/TabFirstActivity.java
@@ -0,0 +1,23 @@
+package com.example.group;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class TabFirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_tab_first);
+
+        // 根据标签栏传来的参数拼接文本字符串
+        String desc = String.format("我是%s页面，来自%s",
+                "首页", getIntent().getExtras().getString("tag"));
+        TextView tv_first = findViewById(R.id.tv_first);
+        Log.d("TabFirstActivity", "getId=" + tv_first.getId());
+        tv_first.setText(desc);
+    }
+}
