7edd07848392faa900896026626679eae4289c29
xiafuxiangfei <xiafuxiangfei@sina.com>
2020-08-17 21:02:53 +0800

    广播：背景变色

--- 5 senior/app/src/main/java/com/example/senior/fragment/DynamicFragment.java
+++ 5 senior/app/src/main/java/com/example/senior/fragment/DynamicFragment.java
@@ -7,7 +7,6 @@ import com.example.senior.R;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-//import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
