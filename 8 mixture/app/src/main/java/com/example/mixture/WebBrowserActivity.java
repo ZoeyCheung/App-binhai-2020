21e6584d920271a5288105b5d1753a56c2cd358d
xiafuxiangfei <xiafuxiangfei@sina.com>
2020-08-26 19:01:42 +0800

    web调用js

---
+++ 8 mixture/app/src/main/java/com/example/mixture/WebBrowserActivity.java
@@ -0,0 +1,14 @@
+package com.example.mixture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebBrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_browser);
+    }
+}
