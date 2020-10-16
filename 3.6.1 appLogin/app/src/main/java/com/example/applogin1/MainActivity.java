1bc2750e11a506f751abcb14656f64cc1777329c
xiafuxiangfei <xiafuxiangfei@sina.com>
2020-10-16 18:26:36 +0800

    忘记密码

--- 3.6.1 appLogin/app/src/main/java/com/example/applogin1/MainActivity.java
+++ 3.6.1 appLogin/app/src/main/java/com/example/applogin1/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
